2fc1ab41d6a54db74a74063c19a0153743f66524
dispotobrett <dispotobrett@gmail.com>
2020-09-16 00:06:28 -0700

    remove venv from git repo

--- backend/install.sh
+++ backend/install.sh
@@ -4,6 +4,6 @@ python3 -m venv ./.venv
 
 ./.venv/bin/pip install --upgrade setuptools
 ./.venv/bin/pip install Django
-./.venv/bin/pip install mod-wsgi
+#./.venv/bin/pip install mod-wsgi
 
 echo "To activate your shell's python venv, please run the following command (without quotes): 'source ./.venv/bin/activate'"
