1f8178a35d9ea5d3c74b217b76c976fbe5fb28c9
tanishababic <tanisha.babic@sjsu.edu>
2020-09-18 15:01:02 -0700

    final profile prototype

--- backend/app/views.py
+++ backend/app/views.py
@@ -6,4 +6,10 @@ def index(request):
     return HttpResponse("<h1>Frozen Frogs 🐸🐸🐸🐸🐸🐸🐸🐸🐸</h1><br>Time:{0}".format(datetime.now()))
 
 def profile(request): 
-	return render(request, 'profile.html')
+	name = "Jane Doe"
+	email = "jane.doe@sjsu.edu"
+	context = {
+		'name': name,
+		'email': email
+	}
+	return render(request, 'profile.html', context)
