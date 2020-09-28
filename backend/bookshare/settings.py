162f8636eb43100a96af90e8ef73d7a0ab768a9e
Steven Myers <myers9s123@gmail.com>
2020-09-28 15:27:11 -0700

    chore: setup docker-compose with mysql container

--- backend/bookshare/settings.py
+++ backend/bookshare/settings.py
@@ -86,6 +86,9 @@ DATABASES = {
 	'USER': 'frogs',
 	'PASSWORD': 'frogs',
 	'HOST': '',#Just use localhost
+        # For docker container:
+        #'HOST': '0.0.0.0',
+        #'PORT': 3306
     }
 }
 
