e2fb9813c99edfaed7f8d77f0091fe8bcde122b4
Steven Myers <myers9s123@gmail.com>
2020-11-09 16:15:37 -0800

    test: update tests.py with model test for List_Book model

--- backend/app/tests.py
+++ backend/app/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from unittest import TestCase as UnitTestCase
 
-# Create your tests here.
+from app.models import *
+
+# Note: Once tests start accumulating in this file, it is a good practice
+# to make tests its own python package (i.e. directory) for organization
+
+# Note: Tests involving database access should inherit from django.test.TestCase
+# Others may use UnitTest
+
+# Note: A temporary database is used when running tests and it is disposed of
+# after tests have finished running
+
+class ListBookTestCase(TestCase):
+    def setUp(self):
+        List_Book.objects.create(
+            title='Hello World',
+            author='Bob Smith',
+            isbn='123456789',
+            subject='Computer Programming',
+            class_used='CS49J'
+        )
+
+    def test_book_exists(self):
+        book = List_Book.objects.get(title='Hello World')
+        self.assertIs(book is not None, True)
