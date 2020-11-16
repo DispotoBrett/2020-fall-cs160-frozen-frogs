ced5cdea9133e7f355d1d5ea24a08ed984f32326
dispotobrett <dispotobrett@gmail.com>
2020-11-15 16:15:36 -0800

    Update readme for tests

--- README.md
+++ README.md
@@ -9,14 +9,15 @@
 	* 5.2. [Configured Containers (docker-compose.yml)](#ConfiguredContainersdocker-compose.yml)
 * 6. [Run in development mode](#Runindevelopmentmode)
 * 7. [Deploy](#Deploy)
-* 8. [Notes](#Notes)
+* 8. [Tips and Tricks](#Notes)
 * 9. [Default Configurations](#Defaultconfigurations)
-* 10. [Regression Tests](#RegressionTests)
+* 10. [Unit Tests](#UnitTests)
+* 11. [Regression Tests](#RegressionTests)
 
 ##  1. <a name='BuildPrerequisites'></a>Build Prerequisites:
 - Python 3.5+ installed
 
-- Python [venv](https://docs.python.org/3/library/venv.html) installed 
+- Python [venv](httpNs://docs.python.org/3/library/venv.html) installed 
 
 - bash or zsh 
 
@@ -119,7 +120,7 @@ To deploy the application on an apache web server:
 
 - (Follow the instructions on installing and configuring [mod_wsgi](https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/modwsgi/). It will invovle installing a few packages and messing with a few config files. 
 
-##  8. <a name='Notes'></a>Notes
+##  8. <a name='Notes'></a>Tips and Tricks
 - The django admin username and password are both `frogs`
 - Create an admin account by running `python manage.py createsuperuser` and following the prompt
 
@@ -130,8 +131,9 @@ To deploy the application on an apache web server:
 	1. Username: DemoSeller
 	2. Username: DemoBuyer
 	- The password for both is 'sjsu' (no quotes)
-	
-##  10. <a name='RegressionTests'></a>Regression Tests
+##  10. <a name='UnitTests'></a>Unit Tests
+To run unit tests for the models and views, you can run `./manage.py test`.
+##  11. <a name='RegressionTests'></a>Regression Tests
 - The system-level automated tests use the following tools:
 	- `npm` and `node` for most things
 	- `mocha` for the test framework
---
+++ backend/app/tests/test_models.py
@@ -0,0 +1,101 @@
+from django.test import TestCase
+from unittest import TestCase as UnitTestCase
+
+from app.models import *
+from .utils import *
+
+# Note: Once tests start accumulating in this file, it is a good practice
+# to make tests its own python package (i.e. directory) for organization
+
+# Note: Tests involving database access should inherit from django.test.TestCase
+# Others may use UnitTest
+
+# Note: A temporary database is used when running tests and it is disposed of
+# after tests have finished running
+
+
+class ListBookTestCase(TestCase):
+    def setUp(self):
+        book = create_test_book()
+        self.book_pk = book.pk
+
+    def test_book_exists(self):
+        book = List_Book.objects.get(pk=self.book_pk)
+        self.assertIs(book is not None, True)
+
+    def test_update_book(self):
+        author_name = 'Bob Smith Jr'
+
+        book = List_Book.objects.get(pk=self.book_pk)
+        book.author = author_name
+        book.save()
+
+        updated_book = List_Book.objects.get(pk=self.book_pk)
+        self.assertEqual(updated_book.author, author_name)
+
+
+class PostingTestCase(TestCase):
+    def setUp(self):
+        user = create_test_user()
+        book = create_test_book()
+        posting = create_test_posting(user, book)
+
+        self.posting_pk = posting.pk
+
+    def test_posting_exists(self):
+        posting = Posting.objects.get(pk=self.posting_pk)
+        self.assertIs(posting is not None, True)
+
+
+class FavoriteTestCase(TestCase):
+    def setUp(self):
+        self.user = create_test_user()
+        self.book = create_test_book()
+        self.posting = create_test_posting(self.user, self.book)
+
+    def test_create_favorite(self):
+        favorite = Favorite.objects.create(user=self.user, posting=self.posting)
+        self.assertIs(favorite is not None, True)
+
+
+class MessageTestCase(TestCase):
+    def setUp(self):
+        user1 = create_test_user()
+        user2 = create_test_user(name='tester2')
+        message = Message.objects.create(
+            to_user=user2,
+            from_user=user1,
+            message_text='Hi there! I would like to purchase your book'
+        )
+
+        self.message_pk = message.pk
+
+    def test_message_exists(self):
+        message = Message.objects.get(pk=self.message_pk)
+        self.assertIs(message is not None, True)
+
+
+class RegisterTestCase(TestCase):
+    def test_create_register(self):
+        register = Register.objects.create(
+            sjsu_id=12345678,
+            sjsu_pw='12345678',
+            name='test account'
+        )
+        self.assertIs(register is not None, True)
+
+
+class ReportTestCase(TestCase):
+    def setUp(self):
+        self.user = create_test_user()
+        self.book = create_test_book()
+        self.posting = create_test_posting(self.user, self.book)
+
+    def test_create_report(self):
+        report = Report.objects.create(
+            user=self.user,
+            posting=self.posting,
+            description='Testing the reporting feature',
+            posting_snapshot=''
+        )
+        self.assertIs(report is not None, True)
